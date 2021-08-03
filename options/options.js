018446e869c4291b5b51fed72d3242d8bdf1fa07
Crowdbotics <team@crowdbotics.com>
2021-08-03 12:56:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "little_truth_29390Identifier",
+  url: "https://little_truth_29390Identifier.botics.co",
+  api: "https://little_truth_29390Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
